54ebbc19714d801ce5bcff14ae68e363dc4dc312
manishganta <manishteja.ganta@gmail.com>
2017-02-26 16:25:05 -0500

    MongoDB
    Connecting to mongo db

---
+++ mongoDB/src/main/java/com/manish/mongodb/CustomerRepository.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.manish.mongodb;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * @author ManishTeja
+ *
+ */
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
